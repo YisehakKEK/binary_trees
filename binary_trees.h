1eb5fe2ab77e1f039acf1de5719650428e884b35
YisehakKEK <yisehakschool@gmail.com>
2024-12-05 12:12:12 +0300

    Update header file with binary_tree_insert_right prototype

--- binary_trees.h
+++ binary_trees.h
@@ -2,7 +2,6 @@
 #define BINARY_TREES_H
 
 #include <stddef.h>
-#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -25,7 +24,10 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-/* Function prototype */
+/* Prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 
 #endif /* BINARY_TREES_H */
+
